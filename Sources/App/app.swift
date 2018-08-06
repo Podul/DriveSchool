e582adda91a4f7f2db2430b1603c067ea25f1176
Podul <ylpodul@gmail.com>
2018-08-06 18:52:15 +0800

    支持修改hostname等

--- Sources/App/app.swift
+++ Sources/App/app.swift
@@ -11,11 +11,33 @@ public func app(_ env: Environment) throws -> Application {
         let con = NIOServerConfig.default(port: 12138)
         services.register(con)
     #endif
-    
+    let con = NIOServerConfig.default(port: 12138)
+    services.register(con)
     
     
     try configure(&config, &env, &services)
     let app = try Application(config: config, environment: env, services: services)
     try boot(app)
     return app
+}
+
+extension NIOServerConfig {
+    static func loadDefaultConfig() -> NIOServerConfig {
+        
+        struct ServerConfig: Codable {
+            var hostname: String?
+            var port: Int?
+        }
+        
+        #if os(Linux)
+        let path = "/home/project/Private/config.json"
+        if let data = FileManager.default.contents(atPath: path) {
+            if let config = try? JSONDecoder().decode(ServerConfig.self, from: data) {
+                return NIOServerConfig.default(hostname: config.hostname, port: config.port)
+            }
+        }
+        #endif
+        return NIOServerConfig.default()
+        
+    }
 }
